748dc228e90261044ef63f8ac4a73987e17c9b34
lee <leeringham@gmail.com>
2020-03-09 21:02:41 -0600

    formatted code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 mod vector2;
 
-
 fn main() {
 	let mut v = vector2::new(1.0, 0.0);
 	let v2 = vector2::new(0.2, 3.0);
 
-	let half:f32 =  2.0;
+	let half: f32 = 2.0;
 	v.print();
 	for _x in 0..4 {
 		v.rotate(std::f32::consts::PI / half);
--- src/vector2.rs
+++ src/vector2.rs
@@ -1,6 +1,5 @@
 use std::ops; // Used for operator overloading
 
-
 #[derive(Copy, Clone)]
 pub struct Vector2 {
 	pub x: f32,
@@ -10,7 +9,7 @@ pub struct Vector2 {
 // Constructor ================
 #[allow(dead_code)]
 pub fn new(x: f32, y: f32) -> Vector2 {
-	return Vector2{x: x, y: y};
+	return Vector2 { x: x, y: y };
 }
 
 // Implementations ================
@@ -29,14 +28,13 @@ impl Vector2 {
 	pub fn zero(&mut self) {
 		self.x = 0.0;
 		self.y = 0.0;
-    }
-	
+	}
 	pub fn dot(&self, rhs: Vector2) -> f32 {
 		return self.x * rhs.x + self.y * rhs.y;
 	}
 
-    pub fn print(&self) {
-        println!("({}, {})", self.x, self.y);
+	pub fn print(&self) {
+		println!("({}, {})", self.x, self.y);
 	}
 
 	pub fn rotate(&mut self, rad: f32) {
@@ -50,25 +48,33 @@ impl Vector2 {
 // Operators ================
 #[allow(dead_code)]
 impl ops::Add<Vector2> for Vector2 {
-    type Output = Vector2;
-    fn add(self, rhs: Vector2) -> Vector2 {
-		return Vector2{x: self.x + rhs.x, y: self.y + rhs.y};
-    }
+	type Output = Vector2;
+	fn add(self, rhs: Vector2) -> Vector2 {
+		return Vector2 {
+			x: self.x + rhs.x,
+			y: self.y + rhs.y,
+		};
+	}
 }
 
 #[allow(dead_code)]
 impl ops::Sub<Vector2> for Vector2 {
-    type Output = Vector2;
-    fn sub(self, rhs: Vector2) -> Vector2 {
-		return Vector2{x: self.x - rhs.x, y: self.y - rhs.y};
-    }
+	type Output = Vector2;
+	fn sub(self, rhs: Vector2) -> Vector2 {
+		return Vector2 {
+			x: self.x - rhs.x,
+			y: self.y - rhs.y,
+		};
+	}
 }
 
 #[allow(dead_code)]
 impl ops::Mul<Vector2> for Vector2 {
-    type Output = Vector2;
-    fn mul(self, rhs: Vector2) -> Vector2 {
-		return Vector2{x: self.x * rhs.x, y: self.y * rhs.y};
-    }
+	type Output = Vector2;
+	fn mul(self, rhs: Vector2) -> Vector2 {
+		return Vector2 {
+			x: self.x * rhs.x,
+			y: self.y * rhs.y,
+		};
+	}
 }
-
