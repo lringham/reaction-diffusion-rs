924d9ea1cd2132e9d50be781f0a6b58859711e67
lee <leeringham@gmail.com>
2020-03-11 23:53:45 -0600

    attempted to clean up code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 #![allow(non_snake_case)]
 
-pub struct RD {
-	pub activator_ : Vec<f32>,
-	pub substrate_ : Vec<f32>,
+pub struct ActivatorSubstrate {
+	pub activator_: Vec<f32>,
+	pub substrate_: Vec<f32>,
+}
+
+struct GrayScottParameters {
+	F: f32,
+	k: f32,
+	dt: f32,
+	Da: f32,
+	Ds: f32,
 }
 
 fn get_index(x: u32, y: u32, width: u32) -> usize {
@@ -10,26 +18,32 @@ fn get_index(x: u32, y: u32, width: u32) -> usize {
 }
 
 fn laplacian(concentrations: &Vec<f32>, x: u32, y: u32, width: u32, height: u32) -> f32 {
-	// repeating BCs
-	let left  = if x == 0          { width - 1 }  else { x-1 };
-	let right = if x >= width - 1  { 0 }          else { x+1 };	
-	let up    = if y == 0          { height - 1 } else { y-1 };
-	let down  = if y >= height - 1 { 0 }          else { y+1 };
+	// Repeating BCs
+	let left = if x == 0 { width - 1 } else { x - 1 };
+	let right = if x >= width - 1 { 0 } else { x + 1 };
+	let up = if y == 0 { height - 1 } else { y - 1 };
+	let down = if y >= height - 1 { 0 } else { y + 1 };
 
-	// calculate laplacian
-	return -4.0 * concentrations[get_index(x,     y,    width)] + 
-				  concentrations[get_index(left,  y,    width)] +
-				  concentrations[get_index(x,     up,   width)] + 
-				  concentrations[get_index(x,     down, width)] +
-				  concentrations[get_index(right, y,    width)];
+	// Calculate laplacian
+	return -4.0 * concentrations[get_index(x, y, width)]
+		+ concentrations[get_index(left, y, width)]
+		+ concentrations[get_index(x, up, width)]
+		+ concentrations[get_index(x, down, width)]
+		+ concentrations[get_index(right, y, width)];
 }
 
-fn concentration_to_color(concentration : f32) -> char {
+fn concentration_to_color(concentration: f32) -> char {
 	let color_map = " .:;=+xX%$";
 
 	let mut concentration = 9.0 * (concentration / 0.4);
 	concentration = concentration.round();
-	concentration = if concentration < 0.0 { 0.0 } else if concentration > 9.0 { 9.0 } else { concentration };
+	concentration = if concentration < 0.0 {
+		0.0
+	} else if concentration > 9.0 {
+		9.0
+	} else {
+		concentration
+	};
 
 	let mut i = concentration as usize;
 	if i >= color_map.chars().count() {
@@ -39,35 +53,59 @@ fn concentration_to_color(concentration : f32) -> char {
 	return color_map.chars().nth(i).unwrap();
 }
 
-fn main() {
+fn gray_scott(
+	read_from: &ActivatorSubstrate,
+	write_to: &mut ActivatorSubstrate,
+	width: u32,
+	height: u32,
+	params: &GrayScottParameters
+) {
+	for x in 0..width {
+		for y in 0..height {
+			let index = get_index(x, y, width);
+			let a = read_from.activator_[index];
+			let s = read_from.substrate_[index];
+			let lap_a = laplacian(&read_from.activator_, x, y, width, height);
+			let lap_s = laplacian(&read_from.substrate_, x, y, width, height);
 
+			write_to.activator_[index] =
+				(params.Da * lap_a + s * a * a - (params.F + params.k) * a) * params.dt + a;
+			write_to.substrate_[index] =
+				(params.Ds * lap_s - s * a * a + params.F * (1.0 - s)) * params.dt + s;
+		}
+	}
+}
+
+fn main() {
 	// Domain size
-	let width  : u32 = 100;
-	let height : u32 = 100;
+	let width: u32 = 100;
+	let height: u32 = 100;
+	let iters: u32 = 100000;
 
 	// Reaction-diffusion parameters for the Gray-Scott model
-	let F 	   : f32 = 0.042;
-	let k      : f32 = 0.063;
-	let dt     : f32 = 0.2;
-	let Da     : f32 = 0.25;
-	let Ds     : f32 = 0.5;
-	let iters  : u32 = 100000;
-
-	// Initialize the domain
-	let mut reaction_diffusion0 = RD{
-		activator_: vec![0.0; (width*height) as usize],
-		substrate_: vec![1.0; (width*height) as usize],
+	let parameters = GrayScottParameters {
+		F: 0.042,
+		k: 0.063,
+		dt: 0.2,
+		Da: 0.25,
+		Ds: 0.5,
 	};
 
-	let mut reaction_diffusion1 = RD{
-		activator_: vec![0.0; (width*height) as usize],
-		substrate_: vec![1.0; (width*height) as usize],
+	// Initialize the domain
+	let mut reaction_diffusion0 = ActivatorSubstrate {
+		activator_: vec![0.0; (width * height) as usize],
+		substrate_: vec![1.0; (width * height) as usize],
+	};
+
+	let mut reaction_diffusion1 = ActivatorSubstrate {
+		activator_: vec![0.0; (width * height) as usize],
+		substrate_: vec![1.0; (width * height) as usize],
 	};
 
 	// Add a 10x10 square of activator
 	for x in 0..10 {
 		for y in 0..10 {
-			let index 	      = get_index(x, y, width);
+			let index = get_index(x, y, width);
 			reaction_diffusion0.activator_[index] = 0.5;
 			reaction_diffusion1.activator_[index] = 0.5;
 		}
@@ -75,47 +113,29 @@ fn main() {
 
 	// Run the simulation
 	let mut mode = false;
-	for _i in 0..iters
-	{
-		if mode 
-		{ 
-			let read_from = &mut reaction_diffusion1;
-			let write_to  = &mut reaction_diffusion0;	
-			
-			for x in 0..width {
-				for y in 0..height {
-					let index = get_index(x, y, width);
-					let a     = read_from.activator_[index];
-					let s     = read_from.substrate_[index];
-					let lap_a = laplacian(&read_from.activator_, x, y, width, height);
-					let lap_s = laplacian(&read_from.substrate_, x, y, width, height);
-
-					write_to.activator_[index] = (Da * lap_a + s*a*a - (F+k)*a)   * dt + a;
-					write_to.substrate_[index] = (Ds * lap_s - s*a*a + F*(1.0-s)) * dt + s;
-				}
-			}
+	for _i in 0..iters {
+		if mode {
+			gray_scott(
+				&reaction_diffusion0,
+				&mut reaction_diffusion1,
+				width,
+				height,
+				&parameters
+			);
 		} else {
-			let read_from = &mut reaction_diffusion0;
-			let write_to  = &mut reaction_diffusion1;
-			
-			for x in 0..width {
-				for y in 0..height {
-					let index = get_index(x, y, width);
-					let a     = read_from.activator_[index];
-					let s     = read_from.substrate_[index];
-					let lap_a = laplacian(&read_from.activator_, x, y, width, height);
-					let lap_s = laplacian(&read_from.substrate_, x, y, width, height);
-
-					write_to.activator_[index] = (Da * lap_a + s*a*a - (F+k)*a)   * dt + a;
-					write_to.substrate_[index] = (Ds * lap_s - s*a*a + F*(1.0-s)) * dt + s;
-				}
-			}
+			gray_scott(
+				&reaction_diffusion1,
+				&mut reaction_diffusion0,
+				width,
+				height,
+				&parameters
+			);
 		}
 		mode = !mode;
 	}
 
 	// Display the result
-	// Draw border
+	// Draw boarder
 	for _i in 0..width {
 		print!("_");
 	}
@@ -124,141 +144,16 @@ fn main() {
 	// Draw the pattern
 	for x in 0..height {
 		for y in 0..width {
-			let index 	      = get_index(x, y, width);
+			let index = get_index(x, y, width);
 			let concentration = reaction_diffusion0.activator_[index];
 			print!("{}", concentration_to_color(concentration));
 		}
 		print!("\n");
 	}
 
-	// Draw border
+	// Draw boarder
 	for _i in 0..width {
 		print!("_");
 	}
 	print!("\n");
 }
-
-
-// #![allow(non_snake_case)]
-
-// pub struct RD {
-// 	pub activator_ : Vec<f32>,
-// 	pub substrate_ : Vec<f32>,
-// }
-
-// fn get_index(x: u32, y: u32, width: u32) -> usize {
-// 	return (y * width + x) as usize;
-// }
-
-// fn laplacian(concentrations: &Vec<f32>, x: u32, y: u32, width: u32, height: u32) -> f32 {
-// 	// repeating BCs
-// 	let left  = if x == 0          { width - 1 }  else { x-1 };
-// 	let right = if x >= width - 1  { 0 }          else { x+1 };	
-// 	let up    = if y == 0          { height - 1 } else { y-1 };
-// 	let down  = if y >= height - 1 { 0 }          else { y+1 };
-
-// 	// calculate laplacian
-// 	return -4.0 * concentrations[get_index(x,     y,    width)] + 
-// 				  concentrations[get_index(left,  y,    width)] +
-// 				  concentrations[get_index(x,     up,   width)] + 
-// 				  concentrations[get_index(x,     down, width)] +
-// 				  concentrations[get_index(right, y,    width)];
-// }
-
-// fn concentration_to_color(concentration : f32) -> char {
-// 	let color_map = " .:;=+xX%$";
-
-// 	let mut concentration = 9.0 * (concentration / 0.4);
-// 	concentration = concentration.round();
-// 	concentration = if concentration < 0.0 { 0.0 } else if concentration > 9.0 { 9.0 } else { concentration };
-
-// 	let mut i = concentration as usize;
-// 	if i >= color_map.chars().count() {
-// 		i = color_map.chars().count() - 1;
-// 	}
-
-// 	return color_map.chars().nth(i).unwrap();
-// }
-
-// fn main() {
-
-// 	// Domain size
-// 	let width  : u32 = 100;
-// 	let height : u32 = 100;
-
-// 	// Reaction-diffusion parameters for the Gray-Scott model
-// 	let F 	   : f32 = 0.042;
-// 	let k      : f32 = 0.063;
-// 	let dt     : f32 = 0.2;
-// 	let Da     : f32 = 0.25;
-// 	let Ds     : f32 = 0.5;
-// 	let iters  : u32 = 100000;
-
-// 	// Initialize the domain
-// 	let mut reaction_diffusion = vec![
-// 		RD{
-// 			activator_: vec![0.0; (width*height) as usize],
-// 			substrate_: vec![1.0; (width*height) as usize],
-// 		},
-// 		RD{
-// 			activator_: vec![0.0; (width*height) as usize],
-// 			substrate_: vec![1.0; (width*height) as usize],
-// 		},
-// 	];
-
-// 	// Add a 10x10 square of activator
-// 	for x in 0..10 {
-// 		for y in 0..10 {
-// 			let index 	      = get_index(x, y, width);
-// 			reaction_diffusion[0].activator_[index] = 0.5;
-// 			reaction_diffusion[1].activator_[index] = 0.5;
-// 		}
-// 	}
-
-// 	// Run the simulation
-// 	let mut mode = 0;
-// 	for _i in 0..iters
-// 	{
-// 		let read_from = &mut reaction_diffusion[mode];
-// 		mode = (mode + 1) % 2;
-// 		let write_to  = &mut reaction_diffusion[mode];
-		
-// 		for x in 0..width {
-// 			for y in 0..height {
-// 				let index = get_index(x, y, width);
-// 				let a     = read_from.activator_[index];
-// 				let s     = read_from.substrate_[index];
-// 				let lap_a = laplacian(&read_from.activator_, x, y, width, height);
-// 				let lap_s = laplacian(&read_from.substrate_, x, y, width, height);
-
-// 				write_to.activator_[index] = (Da * lap_a + s*a*a - (F+k)*a)   * dt + a;
-// 				write_to.substrate_[index] = (Ds * lap_s - s*a*a + F*(1.0-s)) * dt + s;
-// 			}
-// 		}
-		
-		
-// 	}
-
-// 	// Display the result
-// 	// Draw border
-// 	for _i in 0..width {
-// 		print!("_");
-// 	}
-// 	print!("\n");
-
-// 	// Draw the pattern
-// 	for x in 0..height {
-// 		for y in 0..width {
-// 			let index 	      = get_index(x, y, width);
-// 			let concentration = reaction_diffusion0.activator_[index];
-// 			print!("{}", concentration_to_color(concentration));
-// 		}
-// 		print!("\n");
-// 	}
-
-// 	// Draw border
-// 	for _i in 0..width {
-// 		print!("_");
-// 	}
-// 	print!("\n");
-// }
