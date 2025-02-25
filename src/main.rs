47370291ede80caedff888ac569bfec87891a474
Lee Ringham <lee@vividtheory.com>
2025-02-25 10:03:35 -0700

    Cleaned up code, added Gauss Seidel

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,34 @@
+#[derive(Debug, Clone)]
 struct ActivatorSubstrate {
-	activator: Vec<f32>,
-	substrate: Vec<f32>,
+    activator: Vec<f32>,
+    substrate: Vec<f32>,
 }
 
 struct GrayScottParameters {
-	f: f32,
-	k: f32,
-	dt: f32,
-	da: f32,
-	ds: f32,
+    f: f32,
+    k: f32,
+    dt: f32,
+    da: f32,
+    ds: f32,
 }
 
 fn get_index(x: usize, y: usize, width: usize) -> usize {
-	return (y * width + x) as usize;
+    y * width + x
 }
 
 fn laplacian(concentrations: &[f32], x: usize, y: usize, width: usize, height: usize) -> f32 {
-	// Repeating BCs
-	let left = if x == 0 { width - 1 } else { x - 1 };
-	let right = if x >= width - 1 { 0 } else { x + 1 };
-	let up = if y == 0 { height - 1 } else { y - 1 };
-	let down = if y >= height - 1 { 0 } else { y + 1 };
+    // Repeating BCs
+    let left = if x == 0 { width - 1 } else { x - 1 };
+    let right = if x >= width - 1 { 0 } else { x + 1 };
+    let up = if y == 0 { height - 1 } else { y - 1 };
+    let down = if y >= height - 1 { 0 } else { y + 1 };
 
-	// Calculate laplacian
-	return -4.0 * concentrations[get_index(x, y, width)]
-		+ concentrations[get_index(left, y, width)]
-		+ concentrations[get_index(x, up, width)]
-		+ concentrations[get_index(x, down, width)]
-		+ concentrations[get_index(right, y, width)];
+    // Calculate laplacian
+    -4.0 * concentrations[get_index(x, y, width)]
+        + concentrations[get_index(left, y, width)]
+        + concentrations[get_index(x, up, width)]
+        + concentrations[get_index(x, down, width)]
+        + concentrations[get_index(right, y, width)]
 }
 
 fn concentration_to_color(concentration: f32) -> char {
@@ -37,201 +38,124 @@ fn concentration_to_color(concentration: f32) -> char {
         .get(concentration.round() as usize)
         .or_else(|| color_map.last())
         .unwrap();
-
     byte.into()
 }
 
 fn gray_scott(
-	read_from: &ActivatorSubstrate,
-	write_to: &mut ActivatorSubstrate,
-	width: usize,
-	height: usize,
-	params: &GrayScottParameters
+    read_from: &ActivatorSubstrate,
+    write_to: &mut ActivatorSubstrate,
+    width: usize,
+    height: usize,
+    params: &GrayScottParameters,
 ) {
-	for x in 0..width {
-		for y in 0..height {
-			let index = get_index(x, y, width);
-			let a = read_from.activator[index];
-			let s = read_from.substrate[index];
-			let lap_a = laplacian(&read_from.activator, x, y, width, height);
-			let lap_s = laplacian(&read_from.substrate, x, y, width, height);
+    for x in 0..width {
+        for y in 0..height {
+            let index = get_index(x, y, width);
+            let a = read_from.activator[index];
+            let s = read_from.substrate[index];
+            let lap_a = laplacian(&read_from.activator, x, y, width, height);
+            let lap_s = laplacian(&read_from.substrate, x, y, width, height);
 
-			write_to.activator[index] =
-				(params.da * lap_a + s * a * a - (params.f + params.k) * a) * params.dt + a;
-			write_to.substrate[index] =
-				(params.ds * lap_s - s * a * a + params.f * (1.0 - s)) * params.dt + s;
-		}
-	}
+            write_to.activator[index] =
+                (params.da * lap_a + s * a * a - (params.f + params.k) * a) * params.dt + a;
+            write_to.substrate[index] =
+                (params.ds * lap_s - s * a * a + params.f * (1.0 - s)) * params.dt + s;
+        }
+    }
+}
+
+fn gray_scott_gauss_seidel(
+    buff: &mut ActivatorSubstrate,
+    width: usize,
+    height: usize,
+    params: &GrayScottParameters,
+) {
+    for x in 0..width {
+        for y in 0..height {
+            let index = get_index(x, y, width);
+            let a = buff.activator[index];
+            let s = buff.substrate[index];
+            let lap_a = laplacian(&buff.activator, x, y, width, height);
+            let lap_s = laplacian(&buff.substrate, x, y, width, height);
+
+            buff.activator[index] =
+                (params.da * lap_a + s * a * a - (params.f + params.k) * a) * params.dt + a;
+            buff.substrate[index] =
+                (params.ds * lap_s - s * a * a + params.f * (1.0 - s)) * params.dt + s;
+        }
+    }
 }
 
 fn draw_pattern(width: usize, height: usize, concentrations: &[f32]) {
-	// Draw boarder
-	println!("{}", "_".repeat(width));
-
-	// Draw the pattern
-	for x in 0..height {
-		for y in 0..width {
-			let index = get_index(x, y, width);
-			let concentration = concentrations[index];
-			print!("{}", concentration_to_color(concentration));
-		}
-		print!("\n");
-	}
-
-	// Draw boarder
-	println!("{}", "_".repeat(width));
+    println!("{}", "_".repeat(width));
+    for x in 0..height {
+        for y in 0..width {
+            let index = get_index(x, y, width);
+            let concentration = concentrations[index];
+            print!("{}", concentration_to_color(concentration));
+        }
+        println!();
+    }
+    println!("{}", "_".repeat(width));
 }
 
 fn main() {
-	// Domain size
-	let width: usize = 100;
-	let height: usize = 100;
+    // Reaction-diffusion parameters for the Gray-Scott model
+    let parameters = GrayScottParameters {
+        f: 0.042,
+        k: 0.063,
+        dt: 0.4,
+        da: 0.25,
+        ds: 0.5,
+    };
 
-	// Reaction-diffusion parameters for the Gray-Scott model
-	let parameters = GrayScottParameters {
-		f: 0.042,
-		k: 0.063,
-		dt: 0.4,
-		da: 0.25,
-		ds: 0.5,
-	};
+    // Initialize the domain with a square of activator
+    let width: usize = 100;
+    let height: usize = 100;
+    let mut reaction_diffusion = ActivatorSubstrate {
+        activator: vec![0.0; width * height],
+        substrate: vec![1.0; width * height],
+    };
 
-	// Initialize the domain with a square of activator
-	let mut reaction_diffusion0 = ActivatorSubstrate {
-		activator: vec![0.0; (width * height) as usize],
-		substrate: vec![1.0; (width * height) as usize],
-	};
+    for x in 0..width / 10 {
+        for y in 0..height / 10 {
+            let index = get_index(x, y, width);
+            reaction_diffusion.activator[index] = 0.5;
+        }
+    }
 
-	let mut reaction_diffusion1 = ActivatorSubstrate {
-		activator: vec![0.0; (width * height) as usize],
-		substrate: vec![1.0; (width * height) as usize],
-	};
-
-	for x in 0..width/10 {
-		for y in 0..height/10 {
-			let index = get_index(x, y, width);
-			reaction_diffusion0.activator[index] = 0.5;
-			reaction_diffusion1.activator[index] = 0.5;
-		}
-	}
-
-	// Run the simulation
-	let iters: usize = 10000;
-	let mut mode = false;
-	for _ in 0..iters {
-		if mode {
-			gray_scott(&reaction_diffusion0, &mut reaction_diffusion1, width, height, &parameters);
-		} else {
-			gray_scott(&reaction_diffusion1, &mut reaction_diffusion0, width, height, &parameters);
-		}
-		mode = !mode;
-	}
-
-	// Display the result
-	draw_pattern(width, height, &reaction_diffusion0.activator);
+    // Run the simulation
+    let iters: usize = 10000;
+    let use_gauss_seidel = false;
+    if use_gauss_seidel {
+        for _ in 0..iters {
+            gray_scott_gauss_seidel(&mut reaction_diffusion, width, height, &parameters);
+        }
+        draw_pattern(width, height, &reaction_diffusion.activator);
+    } else {
+        let mut reaction_diffusion0 = reaction_diffusion;
+        let mut reaction_diffusion1 = reaction_diffusion0.clone();
+        let mut mode = false;
+        for _ in 0..iters {
+            if mode {
+                gray_scott(
+                    &reaction_diffusion0,
+                    &mut reaction_diffusion1,
+                    width,
+                    height,
+                    &parameters,
+                );
+            } else {
+                gray_scott(
+                    &reaction_diffusion1,
+                    &mut reaction_diffusion0,
+                    width,
+                    height,
+                    &parameters,
+                );
+            }
+            mode = !mode;
+        }
+        draw_pattern(width, height, &reaction_diffusion0.activator);
+    }
 }
-
-/*                                      Output Pattern
-____________________________________________________________________________________________________
-                       ..:=+xX%%%XXxx+=;:...                      ...:;=+xxXX%%%Xx+=:..
-                       .:;=xXX%%XXXx+=;::..                        ..::;=+xXXX%%XXx=;:.
-                      ..:;=xX%%%XXxx+=;:..                          ..:;=+xxXX%%%Xx=;:..
-                      ..:;+xX%%%XXx++;::..                          ..::;++xXX%%%Xx+;:..
-                      ..:;+xX%%%XXx+=;::..                          ..::;=+xXX%%%Xx+;:..
-                      ..:;+xX%%%XXx+=;::..                          ..::;=+xXX%%%Xx+;:..
-                      ..:;+xX%%%XXx++;::..                          ..::;++xXX%%%Xx+;:..
-                      ..:;=xX%%%XXxx+=;:..                          ..:;=+xxXX%%%Xx=;:..
-                       .:;=xXX%%XXXx+=;::..                        ..::;=+xXXX%%XXx=;:.
-                       ..:=+xX%%%XXxx+=;:...                      ...:;=+xxXX%%%Xx+=:..
-                       ..:;=xXX%%XXXx+=;;:..                      ..:;;=+xXXX%%XXx=;:..
-                        ..:=+xXXXXXXXx+=;::..                    ..::;=+xXXXXXXXx+=:..
-                        ..:;=+xXXXXXXXx+=;:...                  ...:;=+xXXXXXXXx+=;:..
-                         ..:;=+xXXXXXXXx+=;:..                  ..:;=+xXXXXXXXx+=;:..
-                         ..::;+xxXXXXXXXx+=;:..                ..:;=+xXXXXXXXxx+;::..
-                          ..:;=+xxXXXXXXXx+;::..              ..::;+xXXXXXXXxx+=;:..
-                           ..:;=+xXXX%%XXx+=;:..              ..:;=+xXX%%XXXx+=;:..
-                           ...:;=+xXXX%%XXx+=;:..            ..:;=+xXX%%XXXx+=;:...
-                            ..::;=+xXXX%%XXx+;:..            ..:;+xXX%%XXXx+=;::..
-                             ..::;=+xXX%%%Xx+=;:..          ..:;=+xX%%%XXx+=;::..
-                              ..:;=+xXXX%%%Xx=;:..          ..:;=xX%%%XXXx+=;:..
-                              ...:;=+xXX%%%Xx+;:..          ..:;+xX%%%XXx+=;:...
-  ......                       ..:;=+xXXX%%XX+=:..          ..:=+XX%%XXXx+=;:..
-...........                    ..::;=+xXX%%%Xx=;:.          .:;=xX%%%XXx+=;::..                    .
-.::::::::....                   ..:;=+xXX%%%Xx=;:..        ..:;=xX%%%XXx+=;:..                   ...
-:;;;;;;;;::....                 ..:;=+xXX%%%Xx=;:..        ..:;=xX%%%XXx+=;:..                 ....:
-===++++===;::...                ..:;=+xXX%%%Xx=;:..        ..:;=xX%%%XXx+=;:..                ...::;
-+xxxxxxxx+==;::...              ..:;=+xXX%%%Xx=;:..        ..:;=xX%%%XXx+=;:..              ...::;==
-xXXXXXXXXxx+=;::...            ...:;=+xXX%%%Xx=;:.          .:;=xX%%%XXx+=;:...            ...::;=+x
-XX%%%%%%XXXx+=;;:...           ..::;=+xXX%%XX+=;..          ..;=+XX%%XXx+=;::..           ...:;;=+xX
-%%%%%%%%%%XXx++=;::...        ...:;=+xxXX%%Xx+=:..          ..:=+xX%%XXxx+=;:...        ...::;=++xXX
-%%%%%%%%%%%XXxx+=;::....    ....::;=+xXX%%%Xx+;:..          ..:;+xX%%%XXx+=;::....    ....::;=+xxXX%
-%XXXXXXXX%%XXXxx+=;::..........::;=+xxXX%%XXx=;:..          ..:;=xXX%%XXxx+=;::..........::;=+xxXXX%
-XXXXXXXXXXXXXXXxx+=;;:::.....:::;=++xXX%%%Xx+=:..            ..:=+xX%%%XXx++=;:::.....:::;;=+xxXXXXX
-XXxxxxxxXXXXXXXXXx+==;;:::::::;;=++xXXX%%XXx+;:..            ..:;+xXX%%XXXx++=;;:::::::;;==+xXXXXXXX
-xxx++++xxxXXXXXXXXx++==;;;;;;;==++xXXX%%%Xx+=;:..            ..:;=+xX%%%XXXx++==;;;;;;;==++xXXXXXXXX
-x+++==+++xxXXXXXXXXxx++=======++xxXXX%%%XXx=;:..              ..:;=xXX%%%XXXxx++=======++xxXXXXXXXXx
-+==;;;;==++xXXXX%XXXXxx+++++++xxxXXX%%%XXx+=::..              ..::=+xXX%%%XXXxxx+++++++xxXXXX%XXXXx+
-=;;::::;;==+xXXX%%XXXXXxxxxxxxxXXXX%%%XXx+=;:..                ..:;=+xXX%%%XXXXxxxxxxxxXXXXX%%XXXx+=
-;::::::::;;=+xXXX%%%XXXXXXXXXXXXX%%%%XXx+=;:..                  ..:;=+xXX%%%%XXXXXXXXXXXXX%%%XXXx+=;
-::......::;;=+xXXX%%%%XXXXXXXXX%%%%%XXx+=;:...                  ...:;=+xXX%%%%%XXXXXXXXX%%%%XXXx+=;;
-..........::;=+xxXX%%%%%%%%%%%%%%%XXXx+=;::..                    ..::;=+xXXX%%%%%%%%%%%%%%%XXxx+=;::
-..      ...::;=++xXX%%%%%%%%%%%%XXXxx+=;::..                      ..::;=+xxXXX%%%%%%%%%%%%XXx++=;::.
-.        ....:;;=+xxXXX%%%%%%XXXXxx+==;:...                        ...:;==+xxXXXX%%%%%%XXXxx+=;;:...
-           ...::;=++xxXXXXXXXXxxx++=;::...                          ...::;=++xxxXXXXXXXXxx++=;::...
-            ...::;;==++xxxxxx+++==;;::...                            ...::;;==+++xxxxxx++==;;::...
-              ...::;;;=========;;:::...                                ...:::;;=========;;;::...
-               ....::::;;;;;;;:::.....                                  .....:::;;;;;;;::::....
-                 ......::::::.......                                      .......::::::......
-                   ..............                                            ..............
-                        ....                                                      ....
-
-
-
-
-
-
-
-
-                        ....                                                      ....
-                   ..............                                            ..............
-                 ......::::::.......                                      .......::::::......
-               ....::::;;;;;;;:::.....                                  .....:::;;;;;;;::::....
-              ...::;;;=========;;:::...                                ...:::;;=========;;;::...
-            ...::;;==++xxxxxx+++==;;::...                            ...::;;==+++xxxxxx++==;;::...
-           ...::;=++xxXXXXXXXXxxx++=;::...                          ...::;=++xxxXXXXXXXXxx++=;::...
-.        ....:;;=+xxXXX%%%%%%XXXXxx+==;:...                        ...:;==+xxXXXX%%%%%%XXXxx+=;;:...
-..      ...::;=++xXX%%%%%%%%%%%%XXXxx+=;::..                      ..::;=+xxXXX%%%%%%%%%%%%XXx++=;::.
-..........::;=+xxXX%%%%%%%%%%%%%%%XXXx+=;::..                    ..::;=+xXXX%%%%%%%%%%%%%%%XXxx+=;::
-::......::;;=+xXXX%%%%XXXXXXXXX%%%%%XXx+=;:...                  ...:;=+xXX%%%%%XXXXXXXXX%%%%XXXx+=;;
-;::::::::;;=+xXXX%%%XXXXXXXXXXXXX%%%%XXx+=;:..                  ..:;=+xXX%%%%XXXXXXXXXXXXX%%%XXXx+=;
-=;;::::;;==+xXXX%%XXXXXxxxxxxxxXXXX%%%XXx+=;:..                ..:;=+xXX%%%XXXXxxxxxxxxXXXXX%%XXXx+=
-+==;;;;==++xXXXX%XXXXxx+++++++xxxXXX%%%XXx+=::..              ..::=+xXX%%%XXXxxx+++++++xxXXXX%XXXXx+
-x+++==+++xxXXXXXXXXxx++=======++xxXXX%%%XXx=;:..              ..:;=xXX%%%XXXxx++=======++xxXXXXXXXXx
-xxx++++xxxXXXXXXXXx++==;;;;;;;==++xXXX%%%Xx+=;:..            ..:;=+xX%%%XXXx++==;;;;;;;==++xXXXXXXXX
-XXxxxxxxXXXXXXXXXx+==;;:::::::;;=++xXXX%%XXx+;:..            ..:;+xXX%%XXXx++=;;:::::::;;==+xXXXXXXX
-XXXXXXXXXXXXXXXxx+=;;:::.....:::;=++xXX%%%Xx+=:..            ..:=+xX%%%XXx++=;:::.....:::;;=+xxXXXXX
-%XXXXXXXX%%XXXxx+=;::..........::;=+xxXX%%XXx=;:..          ..:;=xXX%%XXxx+=;::..........::;=+xxXXX%
-%%%%%%%%%%%XXxx+=;::....    ....::;=+xXX%%%Xx+;:..          ..:;+xX%%%XXx+=;::....    ....::;=+xxXX%
-%%%%%%%%%%XXx++=;::...        ...:;=+xxXX%%Xx+=:..          ..:=+xX%%XXxx+=;:...        ...::;=++xXX
-XX%%%%%%XXXx+=;;:...           ..::;=+xXX%%XX+=;..          ..;=+XX%%XXx+=;::..           ...:;;=+xX
-xXXXXXXXXxx+=;::...            ...:;=+xXX%%%Xx=;:.          .:;=xX%%%XXx+=;:...            ...::;=+x
-+xxxxxxxx+==;::...              ..:;=+xXX%%%Xx=;:..        ..:;=xX%%%XXx+=;:..              ...::;==
-===++++===;::...                ..:;=+xXX%%%Xx=;:..        ..:;=xX%%%XXx+=;:..                ...::;
-:;;;;;;;;::....                 ..:;=+xXX%%%Xx=;:..        ..:;=xX%%%XXx+=;:..                 ....:
-.::::::::....                   ..:;=+xXX%%%Xx=;:..        ..:;=xX%%%XXx+=;:..                   ...
-...........                    ..::;=+xXX%%%Xx=;:.          .:;=xX%%%XXx+=;::..                    .
-  ......                       ..:;=+xXXX%%XX+=:..          ..:=+XX%%XXXx+=;:..
-                              ...:;=+xXX%%%Xx+;:..          ..:;+xX%%%XXx+=;:...
-                              ..:;=+xXXX%%%Xx=;:..          ..:;=xX%%%XXXx+=;:..
-                             ..::;=+xXX%%%Xx+=;:..          ..:;=+xX%%%XXx+=;::..
-                            ..::;=+xXXX%%XXx+;:..            ..:;+xXX%%XXXx+=;::..
-                           ...:;=+xXXX%%XXx+=;:..            ..:;=+xXX%%XXXx+=;:...
-                           ..:;=+xXXX%%XXx+=;:..              ..:;=+xXX%%XXXx+=;:..
-                          ..:;=+xxXXXXXXXx+;::..              ..::;+xXXXXXXXxx+=;:..
-                         ..::;+xxXXXXXXXx+=;:..                ..:;=+xXXXXXXXxx+;::..
-                         ..:;=+xXXXXXXXx+=;:..                  ..:;=+xXXXXXXXx+=;:..
-                        ..:;=+xXXXXXXXx+=;:...                  ...:;=+xXXXXXXXx+=;:..
-                        ..:=+xXXXXXXXx+=;::..                    ..::;=+xXXXXXXXx+=:..
-                       ..:;=xXX%%XXXx+=;;:..                      ..:;;=+xXXX%%XXx=;:..
-____________________________________________________________________________________________________
-*/
